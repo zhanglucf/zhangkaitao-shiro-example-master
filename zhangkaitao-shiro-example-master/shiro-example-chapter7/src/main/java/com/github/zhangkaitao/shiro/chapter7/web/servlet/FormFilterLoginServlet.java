1ac05bbb739decfb6032e2478e16e660c27bbff8
zhanglucf <1617182862@qq.com>
2021-05-13 22:43:29 +0800

    解读EnvironmentLoaderListener

--- zhangkaitao-shiro-example-master/shiro-example-chapter7/src/main/java/com/github/zhangkaitao/shiro/chapter7/web/servlet/FormFilterLoginServlet.java
+++ zhangkaitao-shiro-example-master/shiro-example-chapter7/src/main/java/com/github/zhangkaitao/shiro/chapter7/web/servlet/FormFilterLoginServlet.java
@@ -1,12 +1,7 @@
 package com.github.zhangkaitao.shiro.chapter7.web.servlet;
 
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
-import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.subject.Subject;
-import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
